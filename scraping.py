30dcecd589ffdc000a5b5df1c088702095f2829b
Damien <damien.philippe@edu.esiee.fr>
2023-12-03 12:54:36 +0100

    scraping.py : Récupération des auto écoles du 92.
    Par défaut récupère pour 3 villes aléatoires.

--- scraping.py
+++ scraping.py
@@ -77,3 +77,5 @@ def get_scraping_data(schools_number=3, regenerate=False):
         print("Terminé !")
     else:
         print("Le fichier DrivingSchools existe déjà !")
+
+get_scraping_data(True)
